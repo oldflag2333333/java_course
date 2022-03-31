d9ee8f9c09d1ef607d0d9e9cd07fd8a5d7f86f31
oldflag <oldflag2333333@gmail.com>
2022-03-31 17:37:35 +0800

    course09

---
+++ course09/src/test/java/com/example/course09/Course09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.course09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Course09ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
